9c842ae001fc19bbd415d0e1f019dd39f6d910d8
marcusalten <marcus.cerezuela@alten.es>
2021-08-18 08:17:35 +0200

    Initial  commit

---
+++ br/src/main/java/bus/BsApplication.java
@@ -0,0 +1,11 @@
+package bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BsApplication.class, args);
+    }
+}
